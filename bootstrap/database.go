b7c484299f94e3e4f5d0e4795c1d0e54ae41aa63
Akozi Emmanuel <akoziemmanuel@gmail.com>
2026-04-21 16:39:32 +0100

    Birthday Backend

--- bootstrap/database.go
+++ bootstrap/database.go
@@ -1,73 +1,71 @@
 package bootstrap
 
-import (
-	_ "github.com/go-sql-driver/mysql"
-	"github.com/jmoiron/sqlx"
-	log "github.com/sirupsen/logrus"
-)
+// import (
+	// _ "github.com/go-sql-driver/mysql"
+// )
 
-func NewMySQLDatabase(env *Env) *sqlx.DB {
-	dbHost := env.DBHost
-	dbPort := env.DBPort
-	dbUser := env.DBUser
-	dbPass := env.DBPass
-	dbName := env.DBName
+// func NewMySQLDatabase(env *Env) *sqlx.DB {
+// 	dbHost := env.DBHost
+// 	dbPort := env.DBPort
+// 	dbUser := env.DBUser
+// 	dbPass := env.DBPass
+// 	dbName := env.DBName
 
-	log.Infof("MySQL config: host='%s' port='%s' user='%s' name='%s'", dbHost, dbPort, dbUser, dbName)
+// 	log.Infof("MySQL config: host='%s' port='%s' user='%s' name='%s'", dbHost, dbPort, dbUser, dbName)
 
-	// example connection string: "test:test@(localhost:3306)/test"
+// 	// example connection string: "test:test@(localhost:3306)/test"
 
-	// If no DB host or credentials are provided assume MySQL is not used in this deployment.
-	if dbHost == "" || dbPort == "" || dbUser == "" || dbName == "" {
-		log.Info("MySQL config not provided, skipping MySQL initialization")
-		return nil
-	}
+// 	// If no DB host or credentials are provided assume MySQL is not used in this deployment.
+// 	if dbHost == "" || dbPort == "" || dbUser == "" || dbName == "" {
+// 		log.Info("MySQL config not provided, skipping MySQL initialization")
+// 		return nil
+// 	}
 
-	db, err := sqlx.Connect("mysql", dbUser+":"+dbPass+"@("+dbHost+":"+dbPort+")/"+dbName+"?parseTime=true")
-	if err != nil {
-		log.Error("failed to connect to MySQL: ", err)
-		return nil
-	}
+// 	db, err := sqlx.Connect("mysql", dbUser+":"+dbPass+"@("+dbHost+":"+dbPort+")/"+dbName+"?parseTime=true")
+// 	if err != nil {
+// 		log.Error("failed to connect to MySQL: ", err)
+// 		return nil
+// 	}
 
-	return db
-}
+// 	return db
+// }
 
-func CloseMySqlConnection(client *sqlx.DB) {
-	if client == nil {
-		package bootstrap
+// func CloseMySqlConnection(client *sqlx.DB) {
+// 	if client == nil {
+// 		package bootstrap
 
-		import (
-			_ "github.com/go-sql-driver/mysql"
-			"github.com/jmoiron/sqlx"
-			log "github.com/sirupsen/logrus"
-		)
+// 		import (
+// 			_ "github.com/go-sql-driver/mysql"
+// 			"github.com/jmoiron/sqlx"
+// 			log "github.com/sirupsen/logrus"
+// 		)
 
-		func NewMySQLDatabase(env *Env) *sqlx.DB {
-			dbHost := env.DBHost
-			dbPort := env.DBPort
-			dbUser := env.DBUser
-			dbPass := env.DBPass
-			dbName := env.DBName
+// 		func NewMySQLDatabase(env *Env) *sqlx.DB {
+// 			dbHost := env.DBHost
+// 			dbPort := env.DBPort
+// 			dbUser := env.DBUser
+// 			dbPass := env.DBPass
+// 			dbName := env.DBName
 
-			// example connection string: "test:test@(localhost:3306)/test"
+// 			// example connection string: "test:test@(localhost:3306)/test"
 
-			db, err := sqlx.Connect("mysql", dbUser+":"+dbPass+"@("+dbHost+":"+dbPort+")/"+dbName+"?parseTime=true")
-			if err != nil {
-				log.Fatal(err)
-			}
+// 			db, err := sqlx.Connect("mysql", dbUser+":"+dbPass+"@("+dbHost+":"+dbPort+")/"+dbName+"?parseTime=true")
+// 			if err != nil {
+// 				log.Fatal(err)
+// 			}
 
-			return db
-		}
+// 			return db
+// 		}
 
-		func CloseMySqlConnection(client *sqlx.DB) {
-			if client == nil {
-				return
-			}
+// 		func CloseMySqlConnection(client *sqlx.DB) {
+// 			if client == nil {
+// 				return
+// 			}
 
-			err := client.Close()
-			if err != nil {
-				log.Fatal(err)
-			}
+// 			err := client.Close()
+// 			if err != nil {
+// 				log.Fatal(err)
+// 			}
 
-			log.Info("Connection to MySQL closed.")
-		}
+// 			log.Info("Connection to MySQL closed.")
+// 		}
