df63712ab094f1dfee998081cb4f6eeb036667fc
Akozi Emmanuel <akoziemmanuel@gmail.com>
2026-04-21 16:49:37 +0100

    Birthday Backend

--- bootstrap/app.go
+++ bootstrap/app.go
@@ -7,20 +7,32 @@ import (
 
 type Application struct {
     Env *Env
-    // Don't rely on sqlx if you aren't using MySQL
+    // We remove the hard-coded DB field for now so we don't force a crash
 }
 
 func App() Application {
     app := &Application{}
+    
+    // 1. Load Env
     app.Env = NewEnv()
+    if app.Env == nil {
+        log.Println("WARNING: Env could not be loaded.")
+    }
 
-    // DEBUG: Print what we are reading
-    mongoURI := os.Getenv("MONGO_URI") 
-    log.Printf("DEBUG: The MONGO_URI variable is: '%s'", mongoURI)
-
+    // 2. Debugging: Print to the logs so we can see what's happening in Railway
+    mongoURI := os.Getenv("MONGO_URI")
     if mongoURI == "" {
-        log.Fatal("FATAL: MONGO_URI is empty! Check your Railway Environment Variables.")
+        log.Println("CRITICAL: MONGO_URI is missing from environment variables!")
+    } else {
+        log.Printf("DEBUG: MONGO_URI is set, attempting connection logic...")
+        // If you know the actual function name, you can call it here.
+        // If not, leave this block commented out to break the crash loop.
     }
 
     return *app
+}
+
+func (app *Application) CloseDBConnection() {
+    // No-op for now to prevent panic
+    log.Println("CloseDBConnection called (noop)")
 }
